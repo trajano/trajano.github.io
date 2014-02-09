5b9cebb1dab4678939fefac2d00586f631e207f1
root <root@ai.(none)>
2014-02-08 19:48:15 -0500

    wget --mirror --restrict-file-names=windows --convert-links http://www.trajano.net/

--- wp-login.php
+++ wp-login.php
@@ -8,7 +8,7 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Trajano &rsaquo; Log In</title>
-	<link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=3.8.1' type='text/css' media='all' />
+	<link rel='stylesheet' id='open-sans-css'  href='http://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=3.8.1' type='text/css' media='all' />
 <link rel='stylesheet' id='dashicons-css'  href='http://www.trajano.net/wp-includes/css/dashicons.min.css?ver=3.8.1' type='text/css' media='all' />
 <link rel='stylesheet' id='wp-admin-css'  href='http://www.trajano.net/wp-admin/css/wp-admin.min.css?ver=3.8.1' type='text/css' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='http://www.trajano.net/wp-includes/css/buttons.min.css?ver=3.8.1' type='text/css' media='all' />
@@ -18,14 +18,14 @@
 <![endif]-->
 <script type='text/javascript' src='http://www.trajano.net/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
 <script type='text/javascript' src='http://www.trajano.net/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
-<script type='text/javascript' src='http://www.trajano.net/wp-content/plugins/google-analyticator/external-tracking.min.js?ver=6.4.7.3'></script>
+<script type='text/javascript' src='wp-content/plugins/google-analyticator/external-tracking.min.js@ver=6.4.7.3'></script>
 <meta name='robots' content='noindex,follow' />
 	</head>
 	<body class="login login-action-login wp-core-ui">
 	<div id="login">
 		<h1><a href="http://wordpress.org/" title="Powered by WordPress">Trajano</a></h1>
 	
-<form name="loginform" id="loginform" action="http://www.trajano.net/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -43,7 +43,7 @@
 </form>
 
 <p id="nav">
-	<a href="http://www.trajano.net/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+	<a href="wp-login.php@action=lostpassword" title="Password Lost and Found">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -60,7 +60,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://www.trajano.net/" title="Are you lost?">&larr; Back to Trajano</a></p>
+	<p id="backtoblog"><a href="index.html" title="Are you lost?">&larr; Back to Trajano</a></p>
 	
 	</div>
 
