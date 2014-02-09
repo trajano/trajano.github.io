ea5d2e52b52e32d1e6f18fb64e0c2b9f04ea0ab4
root <root@ai.(none)>
2014-02-08 19:19:09 -0500

    initial

---
+++ wp-login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Trajano &rsaquo; Log In</title>
+	<link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://www.trajano.net/wp-includes/css/dashicons.min.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-admin-css'  href='http://www.trajano.net/wp-admin/css/wp-admin.min.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://www.trajano.net/wp-includes/css/buttons.min.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='colors-fresh-css'  href='http://www.trajano.net/wp-admin/css/colors.min.css?ver=3.8.1' type='text/css' media='all' />
+<!--[if lte IE 7]>
+<link rel='stylesheet' id='ie-css'  href='http://www.trajano.net/wp-admin/css/ie.min.css?ver=3.8.1' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='http://www.trajano.net/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
+<script type='text/javascript' src='http://www.trajano.net/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='http://www.trajano.net/wp-content/plugins/google-analyticator/external-tracking.min.js?ver=6.4.7.3'></script>
+<meta name='robots' content='noindex,follow' />
+	</head>
+	<body class="login login-action-login wp-core-ui">
+	<div id="login">
+		<h1><a href="http://wordpress.org/" title="Powered by WordPress">Trajano</a></h1>
+	
+<form name="loginform" id="loginform" action="http://www.trajano.net/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://www.trajano.net/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://www.trajano.net/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://www.trajano.net/" title="Are you lost?">&larr; Back to Trajano</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
